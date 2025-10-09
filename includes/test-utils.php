340ae27bde420f84421e7cf98dc284e342700aae
100ji-gh <mo2ji.100@gmail.com>
2025-10-09 22:08:03 +0900

    Update test-utils.php

--- includes/test-utils.php
+++ includes/test-utils.php
@@ -1,8 +1,19 @@
 <?php
-if (!function_exists('ts_normalize_bool')) {
-  function ts_normalize_bool($v) {
-    if (is_bool($v)) return $v;
-    $s = strtolower(trim((string)$v));
-    return in_array($s, ['1','true','yes','on'], true);
-  }
+if ( ! function_exists( 'ts_normalize_bool' ) ) {
+	/**
+	 * いろいろな真偽表現を boolean に正規化する。
+	 *
+	 * @param mixed $v 入力値。
+	 * @return bool
+	 */
+	function ts_normalize_bool( $v ) {
+		if ( is_bool( $v ) ) {
+			return $v;
+		}
+
+		$s = strtolower( trim( (string) $v ) );
+
+		// 配列は WordPress 規約に合わせて array() を使用。
+		return in_array( $s, array( '1', 'true', 'yes', 'on' ), true );
+	}
 }
