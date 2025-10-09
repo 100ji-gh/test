e4e6023b63dbcafd0ff7d7660a42284d26b6d24f
100ji-gh <mo2ji.100@gmail.com>
2025-10-09 23:03:43 +0900

    Update wp-tests-config.php

--- wp-tests-config.php
+++ wp-tests-config.php
@@ -18,6 +18,6 @@ $table_prefix = 'wptests_'; // phpcs:ignore WordPress.WP.GlobalVariablesOverride
 define( 'WP_DEBUG', true );
 
 define( 'WP_TESTS_DOMAIN', 'example.org' );
-define( 'WP_TESTS_EMAIL',  'admin@example.org' );
-define( 'WP_TESTS_TITLE',  'Test Blog' );
-define( 'WP_PHP_BINARY',   'php' );
+define( 'WP_TESTS_EMAIL', 'admin@example.org' );
+define( 'WP_TESTS_TITLE', 'Test Blog' );
+define( 'WP_PHP_BINARY', 'php' );
