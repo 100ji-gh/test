86b7eb0cd1c35c5970143ceaf001a141775ab31e
100ji-gh <mo2ji.100@gmail.com>
2025-10-10 01:54:12 +0900

    Update ThemeSwitchTest.php

--- tests/WordPress/ThemeSwitchTest.php
+++ tests/WordPress/ThemeSwitchTest.php
@@ -1,7 +1,14 @@
 <?php
 class ThemeSwitchTest extends WP_UnitTestCase {
-  public function test_switch_theme_to_twentytwentyfour() {
-    switch_theme('twentytwentyfour'); // CI側にテーマがあればOK
-    $this->assertSame('twentytwentyfour', get_stylesheet());
-  }
+	public function test_switch_existing_theme() {
+		$themes = array_keys( wp_get_themes() );
+		$target = in_array( 'twentytwentyfour', $themes, true )
+			? 'twentytwentyfour'
+			: ( in_array( 'twentytwentyfive', $themes, true ) ? 'twentytwentyfive' : ( $themes[0] ?? '' ) );
+
+		$this->assertNotEmpty( $target, 'No themes available in this WordPress build.' );
+
+		switch_theme( $target );
+		$this->assertSame( $target, get_stylesheet() );
+	}
 }
