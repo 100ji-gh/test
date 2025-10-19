8cbf7dac47253ed5eca8167825adfb10bd5c3728
100ji-gh <mo2ji.100@gmail.com>
2025-10-19 19:57:43 +0900

    Update test-plugin.php

--- test-plugin.php
+++ test-plugin.php
@@ -1,19 +1,7 @@
 <?php
 /**
  * Plugin Name: Hide For Guests (CI)
- * Description: Sample filter to demonstrate CI with WordPress.
+ * Description: Sample plugin placeholder for main branch.
  * Author: CI
  * @package hide_for_guests
  */
-
-\add_filter(
-	'the_title',
-	static function ( string $title ): string {
-		if ( ! \is_user_logged_in() ) {
-			return '[guest] ' . $title;
-		}
-		return $title;
-	},
-	10,
-	1
-);
