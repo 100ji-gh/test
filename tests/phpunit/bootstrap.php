cf392d25c603c47b512bd865c9b7d72e2aff5263
100ji-gh <mo2ji.100@gmail.com>
2025-10-20 16:50:09 +0900

    Update bootstrap.php

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,18 +1,13 @@
 <?php
-// WP PHPUnit のブートストラップ
-$_tests_dir = getenv('WP_PHPUNIT__DIR');
-if (! $_tests_dir || ! file_exists($_tests_dir . '/includes/bootstrap.php')) {
-	fwrite(STDERR, "WP_PHPUNIT__DIR invalid\n");
-	exit(1);
+/**
+ * PHPUnit bootstrap for Hide For Guests.
+ *
+ * Loads the plugin after WordPress test suite is bootstrapped by wp-phpunit.
+ *
+ * @package hide_for_guests
+ */
+
+$plugin = dirname( __DIR__, 2 ) . '/test-plugin.php';
+if ( file_exists( $plugin ) ) {
+	require_once $plugin;
 }
-
-require $_tests_dir . '/includes/functions.php';
-
-// テスト対象プラグインを読み込む
-tests_add_filter('muplugins_loaded', static function () {
-	// リポジトリ直下の test-plugin.php を読み込む
-	require_once dirname(__DIR__, 2) . '/test-plugin.php';
-});
-
-// WordPress のテスト環境を起動
-require $_tests_dir . '/includes/bootstrap.php';
